5ebd722e6667df6bb3cf693413aec0fa5fdd440a
ciec20151104691 <s20151104691@admindeiMac-88.local>
2017-05-08 11:18:59 +0800

    增加了减分按钮
    考虑到可能会加错分的可能，所以加上了减分按钮

--- ping pang/ViewController.swift
+++ ping pang/ViewController.swift
@@ -35,6 +35,13 @@ class ViewController: UIViewController {
         }
     
     }
+    @IBAction func subtraction(_ sender: Any) {
+        a = Int(text1.text!)!
+        if(a>0){
+            a = a - 1;
+        }
+        text1.text = ("\(a)");
+    }
     @IBAction func rightadd(_ sender: Any) {
         b = b + 1;
         text2.text = ("\(b)");
@@ -49,6 +56,13 @@ class ViewController: UIViewController {
         
 
     }
+    @IBAction func rightsubtraction(_ sender: Any) {
+        b = Int(text2.text!)!
+        if(b>0){
+            b = b - 1;
+        }
+        text2.text = ("\(b)");
+    }
     @IBAction func add2(_ sender: Any) {
         a = a + 1;
         number1.text = ("\(a)");
@@ -61,17 +75,31 @@ class ViewController: UIViewController {
             number2.text = ("\(b)");
 
         }}
+    @IBAction func subtraction2(_ sender: Any) {
+        a = Int(number1.text!)!
+        if(a>0){
+            a = a - 1;
+        }
+        number1.text = ("\(a)");
+    }
     @IBAction func rightadd2(_ sender: Any) {
-        a = a + 1;
-        number2.text = ("\(a)");
-        if(a >= 11 && a>=2+b){
-            c = c+1;
-            number4.text = ("\(c)");
+        b = b + 1;
+        number2.text = ("\(b)");
+        if(b >= 11 && b>=2+a){
+            d = d+1;
+            number4.text = ("\(d)");
             a = 0;
             b = 0;
-            number1.text=("\(a)");
+            number1.text = ("\(a)");
             number2.text = ("\(b)");
         }}
+    @IBAction func rightsubtraction2(_ sender: Any) {
+        b = Int(number2.text!)!
+        if(b>0){
+            b = b - 1;
+        }
+        number2.text = ("\(b)");
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
